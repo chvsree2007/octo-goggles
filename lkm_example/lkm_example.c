b32d28b4e3011e468510c6d9e33e1682dda6c552
sreekanth <chvsree2007@gmail.com>
2019-08-26 20:43:19 +0530

    Basic cleanup Signed-off-by: 'Venkata Sreekanth Chintada <chvsree2007@gmail.com>'

--- lkm_example/lkm_example.c
+++ lkm_example/lkm_example.c
@@ -25,46 +25,46 @@ static char *msg_ptr;
 
 /* This structure points to all of the device functions */
 static struct file_operations file_ops = {
-.read = device_read,
-.write = device_write,
-.open = device_open,
-.release = device_release
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release
 };
 
 /* When a process reads from our device, this gets called. */
 static ssize_t device_read(struct file *flip, char *buffer, size_t len, loff_t *offset) {
- int bytes_read = 0;
- /* If were at the end, loop back to the beginning */
- if (*msg_ptr == 0) {
- msg_ptr = msg_buffer;
- }
- /* Put data in the buffer */
- while (len && *msg_ptr) {
- /* Buffer is in user data, not kernel, so you cant just reference
- * with a pointer. The function put_user handles this for us */
- put_user(*(msg_ptr++), buffer++);
- len--;
- bytes_read++;
- }
- return bytes_read;
+	int bytes_read = 0;
+	/* If were at the end, loop back to the beginning */
+	if (*msg_ptr == 0) {
+		msg_ptr = msg_buffer;
+}
+/* Put data in the buffer */
+while (len && *msg_ptr) {
+	 /* Buffer is in user data, not kernel, so you cant just reference
+	 * with a pointer. The function put_user handles this for us */
+	 put_user(*(msg_ptr++), buffer++);
+	 len--;
+	 bytes_read++;
+	 }
+	 return bytes_read;
 }
 
 /* Called when a process tries to write to our device */
 static ssize_t device_write(struct file *flip, const char *buffer, size_t len, loff_t *offset) {
- /* This is a read-only device */
- printk(KERN_ALERT "This operation is not supported.\n");
- return -EINVAL;
+	/* This is a read-only device */
+	printk(KERN_ALERT "This operation is not supported.\n");
+	return -EINVAL;
 }
 
 /* Called when a process opens our device */
 static int device_open(struct inode *inode, struct file *file) {
- /* If device is open, return busy */
- if (device_open_count) {
- return -EBUSY;
- }
- device_open_count++;
- try_module_get(THIS_MODULE);
- return 0;
+	/* If device is open, return busy */
+	if (device_open_count) {
+		return -EBUSY;
+	}
+	device_open_count++;
+	try_module_get(THIS_MODULE);
+	return 0;
 }
 
 /* Called when a process closes our device */
@@ -76,19 +76,19 @@ static int device_release(struct inode *inode, struct file *file) {
 }
 
 static int __init lkm_example_init(void) {
- /* Fill buffer with our message */
- strncpy(msg_buffer, EXAMPLE_MSG, MSG_BUFFER_LEN);
- /* Set the msg_ptr to the buffer */
- msg_ptr = msg_buffer;
- /* Try to register character device */
- major_num = register_chrdev(0, "lkm_example", &file_ops);
- if (major_num < 0) {
- printk(KERN_ALERT "Could not register device: %d\n", major_num);
- return major_num;
- } else {
- printk(KERN_INFO "lkm_example module loaded with device major number %d\n", major_num);
- return 0;
- }
+	/* Fill buffer with our message */
+	strncpy(msg_buffer, EXAMPLE_MSG, MSG_BUFFER_LEN);
+	/* Set the msg_ptr to the buffer */
+	msg_ptr = msg_buffer;
+	/* Try to register character device */
+	major_num = register_chrdev(0, "lkm_example", &file_ops);
+	if (major_num < 0) {
+		printk(KERN_ALERT "Could not register device: %d\n", major_num);
+		return major_num;
+	} else {
+		printk(KERN_INFO "lkm_example module loaded with device major number %d\n", major_num);
+		return 0;
+	}
 }
 
 static void __exit lkm_example_exit(void) {
